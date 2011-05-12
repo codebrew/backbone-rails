bb06c839ebcf86f948bfc49dac20286c0d441a53
Ryan Fitzgerald <ryan@gymlogr.com>
2011-05-11 23:23:17 -0400

    updating created javascript function for underscore

--- lib/backbone-rails/underscore.rb
+++ lib/backbone-rails/underscore.rb
@@ -4,11 +4,11 @@ class Underscore
   JST_START = "(function(){"
   JST_END = "}).call(this);"
   NAMESPACE = "JST"
-  SETUP_NAMESPACE = "window.#{NAMESPACE} = window.#{NAMESPACE} || {};"
+  SETUP_NAMESPACE = "this.#{NAMESPACE} = this.#{NAMESPACE} || {};"
   
   def self.render(name, data)
     # template = "#{NAMESPACE}['#{name}'] = _.template( '#{data}' );"
-    template = "window.#{NAMESPACE}['#{name}'] = _.template(\"#{escape_javascript data}\");"
+    template = "this.#{NAMESPACE}['#{name}'] = _.template(\"#{escape_javascript data}\");"
     
     [JST_START, SETUP_NAMESPACE, template, JST_END].join("\n")
   end
