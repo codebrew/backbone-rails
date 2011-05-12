743f760e45a339777e54c8151f282ab86c850608
Ryan Fitzgerald <ryan@gymlogr.com>
2011-05-11 22:09:57 -0400

    creating home controller to test gem

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -3,4 +3,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
 //= require_tree .
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,8 @@
 Dummy::Application.routes.draw do
+  get "home/index"
+  
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
