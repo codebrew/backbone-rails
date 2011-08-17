9897e5a9261d04ce663a0f0b6ad6a76a3bcedde1
Jonathan Otto <jonathan.otto@gmail.com>
2011-08-17 12:23:38 -0500

    if the input element is a checkbox, read and set the checked property instead of using jquery's val()

--- vendor/assets/javascripts/backbone_datalink.js
+++ vendor/assets/javascripts/backbone_datalink.js
@@ -6,16 +6,22 @@
         el = $(this);
         name = el.attr("name");
         model.bind("change:" + name, function() {
-          return el.val(model.get(name));
+          val = model.get(name)
+          if ( el.is("input[type='checkbox']") && (val == true || val == false) )
+            el.prop('checked', val);
+          return el.val(val);
         });
         return $(this).bind("change", function() {
           var attrs;
           el = $(this);
           attrs = {};
-          attrs[el.attr("name")] = el.val();
+          if ( el.is("input[type='checkbox']") )
+            attrs[el.attr("name")] = el.prop('checked');
+          else
+            attrs[el.attr("name")] = el.val();
           return model.set(attrs);
         });
       });
     }
   });
-})(jQuery);
+})(jQuery)
