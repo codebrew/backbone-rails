289516be72f7558c87901b6614b4dde27b5beece
Jonathan Otto <jonathan.otto@gmail.com>
2011-08-21 18:25:37 -0500

    this includes the checkbox prop changes from my last pull request, and adds a check to ensure there's a value on the name attribute of an element before binding to an event (otherwise any input without a name attr will bind to change:undefined). another way of handling this might be using $(":input[name]") instead of $(":input"). either way.

--- vendor/assets/javascripts/backbone_datalink.js
+++ vendor/assets/javascripts/backbone_datalink.js
@@ -5,16 +5,27 @@
         var el, name;
         el = $(this);
         name = el.attr("name");
-        model.bind("change:" + name, function() {
-          return el.val(model.get(name));
-        });
-        return $(this).bind("change", function() {
-          var attrs;
-          el = $(this);
-          attrs = {};
-          attrs[el.attr("name")] = el.val();
-          return model.set(attrs);
-        });
+
+        if(typeof name !== "undefined" && name !== null) {
+          model.bind("change:" + name, function() {
+            val = model.get(name)
+            if ( el.is("input[type='checkbox']") && (val == true || val == false) )
+              el.prop('checked', val);
+            return el.val(val);
+          });
+
+          return $(this).bind("change", function() {
+            var attrs;
+            el = $(this);
+            attrs = {};
+            if ( el.is("input[type='checkbox']") )
+              attrs[el.attr("name")] = el.prop('checked');
+            else
+              attrs[el.attr("name")] = el.val();
+            return model.set(attrs);
+          });
+        }
+
       });
     }
   });
