d7f31e39192c87388e678cb19844942cb5886020
Caleb Brown <cbrown@blenderbox.com>
2011-11-21 15:57:38 -0500

    updated the plugin a touch more

--- vendor/assets/javascripts/backbone_datalink.js
+++ vendor/assets/javascripts/backbone_datalink.js
@@ -2,17 +2,17 @@
   return $.extend($.fn, {
     backboneLink: function(model) {
       return $(this).find(":input").each(function() {
-        var el, name, chng = 'change';
+        var el, name, chng = 'change', attr = 'name';
         el = $(this);
-        name = el.attr("name");
+        name = el.attr(attr);
         model.bind(chng + ":" + name, function onModelChange() {
           return el.val(model.get(name));
         });
-        return $(this).bind(chng, function onAttrChange() {
+        return el.bind(chng, function onAttrChange() {
           var attrs;
           el = $(this);
           attrs = {};
-          attrs[el.attr("name")] = el.val();
+          attrs[el.attr(attr)] = el.val();
           return model.set(attrs);
         }).trigger(chng);
       });
