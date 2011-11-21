d638d4200f628646453632764b49fd2ed05e5b10
Caleb Brown <cbrown@blenderbox.com>
2011-11-21 15:51:02 -0500

    updated the backbone datalink to not use anonymous functions and trigger the change event on loading of the form for fields that don't get changed before submit (pre-populated data/selects)

--- vendor/assets/javascripts/backbone_datalink.js
+++ vendor/assets/javascripts/backbone_datalink.js
@@ -2,19 +2,19 @@
   return $.extend($.fn, {
     backboneLink: function(model) {
       return $(this).find(":input").each(function() {
-        var el, name;
+        var el, name, chng = 'change';
         el = $(this);
         name = el.attr("name");
-        model.bind("change:" + name, function() {
+        model.bind(chng + ":" + name, function onModelChange() {
           return el.val(model.get(name));
         });
-        return $(this).bind("change", function() {
+        return $(this).bind(chng, function onAttrChange() {
           var attrs;
           el = $(this);
           attrs = {};
           attrs[el.attr("name")] = el.val();
           return model.set(attrs);
-        });
+        }).trigger(chng);
       });
     }
   });
