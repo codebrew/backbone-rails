60cded1e099acc33ff0d2171fbaeb61babb72c9b
Trevor Bortins <enabfluw@gmail.com>
2011-12-06 14:17:01 -0800

    Small change to support select boxes

--- vendor/assets/javascripts/backbone_datalink.js
+++ vendor/assets/javascripts/backbone_datalink.js
@@ -1,7 +1,7 @@
 (function($) {
   return $.extend($.fn, {
     backboneLink: function(model) {
-      return $(this).find(":input").each(function() {
+      return $(this).find(":input,select").each(function() {
         var el, name;
         el = $(this);
         name = el.attr("name");
