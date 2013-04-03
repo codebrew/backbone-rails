4465aa10815b03cfbb47dc8208bdd1378857d637
Riley <bx+github@teleological.net>
2013-04-03 16:11:39 -0400

    Updated for backbone 1.0, underscore 1.4.4

--- vendor/assets/javascripts/backbone_rails_sync.js
+++ vendor/assets/javascripts/backbone_rails_sync.js
@@ -2,13 +2,9 @@
   var methodMap = {
     'create': 'POST',
     'update': 'PUT',
+    'patch':  'PATCH',
     'delete': 'DELETE',
     'read'  : 'GET'
-  };
-  
-  var getUrl = function(object) {
-    if (!(object && object.url)) return null;
-    return _.isFunction(object.url) ? object.url() : object.url;
   };
   
   var urlError = function() {
@@ -18,51 +14,88 @@
   Backbone.sync = function(method, model, options) {
     var type = methodMap[method];
 
+    // Default options, unless specified.
+    _.defaults(options || (options = {}), {
+      emulateHTTP: Backbone.emulateHTTP,
+      emulateJSON: Backbone.emulateJSON
+    });
+
     // Default JSON-request options.
-    var params = _.extend({
-      type:         type,
-      dataType:     'json',
-      beforeSend: function( xhr ) {
-        var token = $('meta[name="csrf-token"]').attr('content');
-        if (token) xhr.setRequestHeader('X-CSRF-Token', token);
+    var params = {type: type, dataType: 'json'};
 
-        model.trigger('sync:start');
-      }
-    }, options);
-
-    if (!params.url) {
-      params.url = getUrl(model) || urlError();
+    // Ensure that we have a URL.
+    if (!options.url) {
+      params.url = _.result(model, 'url') || urlError();
     }
 
     // Ensure that we have the appropriate request data.
-    if (!params.data && model && (method == 'create' || method == 'update')) {
+    if (options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')) {
       params.contentType = 'application/json';
 
-      var data = {}
+      var data = {},
+        attrs = options.attrs || model.toJSON(options);
 
-      if(model.paramRoot) {
-        data[model.paramRoot] = model.toJSON();
+      if (model.paramRoot) {
+        data[model.paramRoot] = attrs;
       } else {
-        data = model.toJSON();
+        data = attrs;
       }
 
-      params.data = JSON.stringify(data)
+      params.data = JSON.stringify(data);
+    }
+
+    // For older servers, emulate JSON by encoding the request into an HTML-form.
+    if (options.emulateJSON) {
+      params.contentType = 'application/x-www-form-urlencoded';
+      params.data = params.data ? {model: params.data} : {};
+    }
+
+    // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
+    // And an `X-HTTP-Method-Override` header.
+    if (options.emulateHTTP && (type === 'PUT' || type === 'DELETE' || type === 'PATCH')) {
+      params.type = 'POST';
+      if (options.emulateJSON) params.data._method = type;
+      var beforeSend = options.beforeSend;
+      options.beforeSend = function(xhr) {
+        xhr.setRequestHeader('X-HTTP-Method-Override', type);
+        if (beforeSend) return beforeSend.apply(this, arguments);
+      };
     }
 
     // Don't process data on a non-GET request.
-    if (params.type !== 'GET') {
+    if (params.type !== 'GET' && !params.emulateJSON) {
       params.processData = false;
+    }
+
+    // If we're sending a `PATCH` request, and we're in an old Internet Explorer
+    // that still has ActiveX enabled by default, override jQuery to use that
+    // for XHR instead. Remove this line when jQuery supports `PATCH` on IE8.
+    if (params.type === 'PATCH' && window.ActiveXObject &&
+          !(window.external && window.external.msActiveXFilteringEnabled)) {
+      params.xhr = function() {
+        return new ActiveXObject("Microsoft.XMLHTTP");
+      };
     }
 
     // Trigger the sync end event
     var complete = options.complete;
-    params.complete = function(jqXHR, textStatus) {
+    options.complete = function(jqXHR, textStatus) {
       model.trigger('sync:end');
       if (complete) complete(jqXHR, textStatus);
     };
-    
-    // Make the request.
-    return $.ajax(params);
+
+    var beforeSend = options.beforeSend;
+    options.beforeSend = function( xhr ) {
+      var token = $('meta[name="csrf-token"]').attr('content');
+      if (token) xhr.setRequestHeader('X-CSRF-Token', token);
+      model.trigger('sync:start');
+      if (beforeSend) return beforeSend.apply(this, arguments);
+    };
+
+    // Make the request, allowing the user to override any Ajax options.
+    var xhr = options.xhr = Backbone.ajax(_.extend(params, options));
+    model.trigger('request', model, xhr, options);
+    return xhr;
   }
   
 })(jQuery);
