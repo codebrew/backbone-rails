4efc159a80f8ef84790927b65683c9847741e87c
Manu S Ajith <neo@codingarena.in>
2014-06-30 19:59:15 +0530

    Add backbone.js files above the require_tree if its present in application.js

--- lib/generators/backbone/install/install_generator.rb
+++ lib/generators/backbone/install/install_generator.rb
@@ -15,13 +15,27 @@ module Backbone
       def inject_backbone
         # for JavaScript application.js manifest:        
         if File.exists? "#{Rails.root}/app/assets/javascripts/application.js"
-          append_to_file "app/assets/javascripts/application.js" do
-            "//= require underscore\n//= require backbone\n//= require backbone_rails_sync\n//= require backbone_datalink\n//= require backbone/#{application_name.underscore}\n"
-          end    
+          #add backbone.js files above the require_tree if present
+          if File.readlines("app/assets/javascripts/application.js").grep(/require_tree/).any?
+            inject_into_file "app/assets/javascripts/application.js", before: '//= require_tree .' do
+              "//= require underscore\n//= require backbone\n//= require backbone_rails_sync\n//= require backbone_datalink\n//= require backbone/#{application_name.underscore}\n"
+            end 
+          else
+            append_to_file "app/assets/javascripts/application.js" do
+              "//= require underscore\n//= require backbone\n//= require backbone_rails_sync\n//= require backbone_datalink\n//= require backbone/#{application_name.underscore}\n"
+            end 
+          end   
         # ...or for CoffeeScript application.js.coffee manifest:
-        elsif File.exists? "#{Rails.root}/app/assets/javascripts/application.js.coffee"   
-          append_to_file "app/assets/javascripts/application.js.coffee" do
-            "\n#= require underscore\n#= require backbone\n#= require backbone_rails_sync\n#= require backbone_datalink\n#= require backbone/#{application_name.underscore}\n"
+        elsif File.exists? "#{Rails.root}/app/assets/javascripts/application.js.coffee" 
+          #add backbone.js files above the require_tree if present
+          if File.readlines("app/assets/javascripts/application.js").grep(/require_tree/).any?
+            inject_into_file "app/assets/javascripts/application.js.coffee", before: '#= require_tree .' do
+              "\n#= require underscore\n#= require backbone\n#= require backbone_rails_sync\n#= require backbone_datalink\n#= require backbone/#{application_name.underscore}\n"
+            end
+          else
+            append_to_file "app/assets/javascripts/application.js.coffee" do
+              "\n#= require underscore\n#= require backbone\n#= require backbone_rails_sync\n#= require backbone_datalink\n#= require backbone/#{application_name.underscore}\n"
+            end
           end
         end
       end
